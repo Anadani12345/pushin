4730cb5d64c3351e2ec9e02c4950a397d7e82c50
Anadani12345 <ahmad.anadani@student.put.poznan.pl>
2017-05-28 19:52:49 +0200

    Add files via upload

---
+++ TestAPP.WindowsPhone/MainPage.xaml.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using Windows.UI.Popups;
+using Lumia.Sense;
+using System.Threading.Tasks;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace TestAPP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private StepCounter _stepCounter;
+
+        
+        public MainPage()
+        {
+
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+            
+
+            Window.Current.VisibilityChanged += async (oo, ee) =>
+            {
+                if (ee.Visible)
+                {
+                    if (await CallSensorcoreApiAsync(async () =>
+                    {
+                        if (_stepCounter == null)
+                        {
+                            _stepCounter = await StepCounter.GetDefaultAsync();
+                        }
+                        else
+                        {
+                            await _stepCounter.ActivateAsync();
+                        }
+                    }))
+                    {
+                        await ShowCurrentReading();
+                    }
+                }
+                else
+                {
+                    if (_stepCounter != null) await CallSensorcoreApiAsync(async () =>
+                      await _stepCounter.DeactivateAsync());
+                }
+            };
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+
+        private async Task<bool> CallSensorcoreApiAsync(Func<Task> action)
+        {
+            if (!(await StepCounter.IsSupportedAsync()))
+            {
+                MessageDialog dlg = new MessageDialog("Unfortunately this device does not support SensorCore features");
+
+                await dlg.ShowAsync();
+                Application.Current.Exit();
+            }
+            Exception failure = null;
+
+            try
+            {
+                await action();
+            }
+            catch (Exception e)
+            {
+                failure = e;
+            }
+
+            if (failure != null)
+            {
+                MessageDialog dialog;
+
+                switch (SenseHelper.GetSenseError(failure.HResult))
+                {
+                    case SenseError.LocationDisabled:
+                        dialog = new MessageDialog("Location has been disabled. Do you want to open Location settings now ? ", "Information" );
+        
+                        dialog.Commands.Add(new UICommand("Yes", async cmd => await
+                          SenseHelper.LaunchLocationSettingsAsync()));
+                        dialog.Commands.Add(new UICommand("No"));
+                        await dialog.ShowAsync();
+                        new System.Threading.ManualResetEvent(false).WaitOne(500);
+                        return false;
+
+                    case SenseError.SenseDisabled:
+                        {
+                            var settings = await SenseHelper.GetSettingsAsync();
+
+                            if (settings.Version < 2)
+                            {
+                                dialog = new MessageDialog("Motion data has been disabled. Do you want to open motion data settings now ? ", "Information" );
+                            }
+                            else
+                            {
+                                dialog = new MessageDialog("Places visited has been disabled. Do you want to open motion data settings now ? ", "Information" );
+                            }
+
+                            dialog.Commands.Add(new UICommand("Yes", new
+                                       UICommandInvokedHandler(async (cmd) => await
+                                      SenseHelper.LaunchSenseSettingsAsync())));
+
+                            dialog.Commands.Add(new UICommand("No"));
+                            await dialog.ShowAsync();
+                            new System.Threading.ManualResetEvent(false).WaitOne(500);
+                            return false;
+                        }
+
+                    default:
+                        dialog = new MessageDialog("Failure: " + SenseHelper.GetSenseError(
+                                          failure.HResult), "");
+                        await dialog.ShowAsync();
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        private async Task ShowCurrentReading()
+        {
+            await CallSensorcoreApiAsync(async () =>
+            {
+                var reading = await _stepCounter.GetCurrentReadingAsync();
+                SensorcoreList.Items.Add("Current step counter reading");
+
+                if (reading != null)
+                {
+                    SensorcoreList.Items.Add(reading.Timestamp.ToString());
+                    SensorcoreList.Items.Add("Walk steps = " + reading.WalkingStepCount);
+                    SensorcoreList.Items.Add("Walk time = " + reading.WalkTime.ToString());
+                    SensorcoreList.Items.Add("Run steps = " + reading.RunningStepCount);
+                    SensorcoreList.Items.Add("Run time = " + reading.RunTime.ToString());
+                }
+                else
+                {
+                    SensorcoreList.Items.Add("Data not available");
+                }
+            });
+        }
+    }
+}
